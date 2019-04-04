bd80b2b60e4b5be2025f0a2935a4aa30604c7878
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-04 18:19:40 +0300

    functions are added, last step is local storage

--- script.js
+++ script.js
@@ -1,41 +1,119 @@
 const itemDescription = document.querySelector('#item-description')
 const itemCost = document.querySelector('#item-cost')
-const addButton = document.querySelector('.add')
+const addButton = document.querySelector('.add-button')
 const select = document.querySelector('#selection')
 const option = document.querySelectorAll('option')
-
-// console.log(option[0])
-// console.log(option[1])
-// console.log(option[2])
-
-addButton.addEventListener('click', function (){
+const incomeData = document.querySelector('.income-data')
+const expenseData = document.querySelector('.expense-data')
 
 
-        let selectedValue = select.options[select.selectedIndex].value
-        console.log(selectedValue)
+const accountBalance = {
+    name: 'Koray',
+    lastName: 'Dündar',
+    income: {
+        description:[],
+        amount:[]
+    },
+    expense:{
+        description:[],
+        amount:[]
+    },
+    balance: 0
+}
 
-        if (selectedValue === 'income'){
-            console.log('Income is added')
 
-        } else if (selectedValue === 'expense'){
-            console.log('Expense is added')
 
-        } else {
-            console.log('Please select an option')
 
-        }
+
+//EVENT LISTENER
+addButton.addEventListener('click', createContent)
+addButton.addEventListener('click', checkBalance)
+//EVENT LISTENER
+
+
+
+
+
+
+function createContent() {
+    let selectedValue = select.options[select.selectedIndex].value
+    // console.log(selectedValue)
+
+   
+    
+    
+    if (selectedValue === 'income'){
+        accountBalance.income.description.push(itemDescription.value)
+        accountBalance.income.amount.push(parseInt(itemCost.value))
         
-})
+        const accountBalanceParse = JSON.parse(localStorage.getItem('accountBalance'))
+        accountBalanceParse.income.amount.forEach(element => {
+            
+            incomeData.innerHTML += `<p>${element}</p>`
+             
+        } )
 
+    
+    console.log(accountBalance.income)
 
+        
+    } else if (selectedValue === 'expense'){
+        accountBalance.expense.description.push(itemDescription.value)
+        accountBalance.expense.amount.push(parseInt(itemCost.value))
 
-// console.log(select.value)
+        const accountBalanceParse = JSON.parse(localStorage.getItem('accountBalance'))
+        accountBalanceParse.expense.amount.forEach(element => {
 
-// function add() {
-//     //Get the value from the first input box (Description etc 'Milk')
+            expenseData.innerHTML += `<p>${element}</p>`
+        })
+   
 
-// }
+    
+    console.log(accountBalance.expense)
+        
+    
 
-function checkBalance () {
+    } else {
+        console.log('Please select an option')
+
+    }
+
+    const accountBalanceStringify = JSON.stringify(accountBalance, undefined, 2)
+    console.log(accountBalanceStringify)
+    localStorage.setItem('accountBalance', accountBalanceStringify)
+    console.log(localStorage.getItem('accountBalance'))
     
 }
+
+
+function checkBalance () {
+    let sumIncome = 0;
+    let sumExpense = 0;
+    
+    
+    accountBalance.income.amount.forEach(element => {
+        sumIncome = sumIncome + element           
+        // console.log(sumIncome)
+    })
+    
+    accountBalance.expense.amount.forEach(element => {
+        sumExpense = sumExpense + element
+        // console.log(sumExpense)
+    })
+
+
+    let balance = sumIncome - sumExpense;
+    
+    document.querySelector('.balance-result').textContent=balance
+    console.log(balance);
+    
+    return balance
+}
+
+
+    // const accountBalanceParse = JSON.parse(localStorage.getItem('accountBalance'))
+    // for(let i=0; i <accountBalanceParse.income.amount.length; i++){
+        
+    //     incomeData.innerHTML += `<p>${accountBalanceParse.income.amount[i]}</p>`
+    // }
+
