01d4df7556b62ffac06d71782180d57b6a4ee982
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-08 15:25:08 +0300

    Fixed few bugs

--- script.js
+++ script.js
@@ -11,27 +11,27 @@ const accountBalance = {
     name: 'Koray',
     lastName: 'Dündar',
     incomes: [
-        {
-            description: 'salary',
-            amount: 2000,
-            time: displayDateTime()
-        }, {
-            description: 'Bonus',
-            amount: 500,
-            time: displayDateTime()
-        }
+        // {
+        //     description: 'salary',
+        //     amount: 2000,
+        //     time: displayDateTime()
+        // }, {
+        //     description: 'Bonus',
+        //     amount: 500,
+        //     time: displayDateTime()
+        // }
     ],
     expenses: [
-        {
-            description: 'rent',
-            amount: 7000,
-            time: displayDateTime()
-        }, 
-        {
-            description: 'food',
-            amount: 500,
-            time: displayDateTime()
-        }
+        // {
+        //     description: 'rent',
+        //     amount: 7000,
+        //     time: displayDateTime()
+        // }, 
+        // {
+        //     description: 'food',
+        //     amount: 500,
+        //     time: displayDateTime()
+        // }
 ], 
     addIncome: function (description, amount) {
         //load local storage
@@ -43,7 +43,7 @@ const accountBalance = {
         let json = localStorage.getItem('incomes')
         const retreivedFromLocal = JSON.parse(json);
         retreivedFromLocal.push({description, amount, time});
-        localStorage.setItem('incomes', JSON.stringify(retreivedFromLocal, undefined, 4));
+        localStorage.setItem('incomes', JSON.stringify(retreivedFromLocal), undefined, 4);
         
         console.log(retreivedFromLocal);
         
@@ -56,7 +56,7 @@ const accountBalance = {
         let json = localStorage.getItem('expenses')
         const retreivedFromLocal = JSON.parse(json);
         retreivedFromLocal.push({description, amount, time});
-        localStorage.setItem('expenses', JSON.stringify(retreivedFromLocal, undefined, 4));
+        localStorage.setItem('expenses', JSON.stringify(retreivedFromLocal), undefined, 4);
         
         console.log(retreivedFromLocal);
         
@@ -82,36 +82,24 @@ const accountBalance = {
         return balance;
     },
     getIncomeData: function () {
-        let data = JSON.parse(localStorage.getItem('incomes'));
-        console.log(data);
+        let dataIncome = JSON.parse(localStorage.getItem('incomes'));
         incomeData.innerHTML = ''
-        let result;
-        if(data === null){
-            result = accountBalance.incomes.forEach(data =>{
-            incomeData.innerHTML += `<div class="incomes"><p>${data.description}</p><br>
-            <p>${data.amount}</p><br><p>${data.time}</p><br></div>`;
-            })
-        }else{
-         result = data.forEach(data =>{
+        let result = dataIncome.forEach(data=> {
             incomeData.innerHTML += `<div class="incomes"><p>${data.description}</p><br>
             <p>${data.amount}</p><br><p>${data.time}</p><br></div>`;
         })
-    }
         return result;
-
     },
     getExpenseData: function () {
-        let data = JSON.parse(localStorage.getItem('expenses'));
-        console.log(data);
+        let dataExpense = JSON.parse(localStorage.getItem('expenses'));
         expenseData.innerHTML = ''
-        let result = data.forEach(data =>{
+        let result = dataExpense.forEach(data => {
             expenseData.innerHTML += `<div class="incomes"><p>${data.description}</p><br>
             <p>${data.amount}</p><br><p>${data.time}</p><br></div>`;
         })
+       
         return result;
-
     }
-
 
 }
 
@@ -121,14 +109,14 @@ const accountBalance = {
 // localStorage.setItem('expenses', JSON.stringify(accountBalance.expenses, undefined, 2));
 
 
-console.log(accountBalance.totalIncome());
-console.log(accountBalance.totalExpense());
+// console.log(accountBalance.totalIncome());
+// console.log(accountBalance.totalExpense());
 
 
 //EVENT LISTENER
 addButton.addEventListener('click', function () {
     let selectedValue = select.options[select.selectedIndex].value;
-    console.log(selectedValue)
+    // console.log(selectedValue)
     
     // accountBalance.showContent()
     
@@ -149,96 +137,6 @@ addButton.addEventListener('click', function () {
     accountBalance.getExpenseData();
     
 })
-
-accountBalance.getIncomeData()
-accountBalance.getExpenseData()
-// function  showContent () {
-    
-//     let output =  JSON.parse(localStorage.getItem('incomes')).forEach(element =>{
-        
-//         incomeData.innerHTML += `<div><span>${element.amount}</span></div>`;
-//     })
-//     console.log(output);
-//     return output;
-// }
-// showContent();
-
-
-// function setToLocal(){
-//     let stringifiedAccount = JSON.stringify(accountBalance, undefined, 2);
-//     localStorage.setItem('accountBalance', stringifiedAccount);
-// }
-// setToLocal();
-
-// function getFromLocal(){
-//     let unStringifiedAccount = JSON.parse(localStorage.getItem('accountBalance'));
-//     // let {a,b} = unStringifiedAccount.expense;
-//     let {description, amount} = unStringifiedAccount.income;
-
-//     console.log(unStringifiedAccount.expense);
-//     description.forEach(description =>{
-//         incomeData.innerHTML += `<div><span>${description}</span></div>`;
-//     })
-
-//     amount.forEach(element =>{
-//         incomeData.innerHTML += `<div><span>${element}</span></div>`;
-//     })
-
-//     // a.forEach(element =>{
-//     //     expenseData.innerHTML += `<div><span>${element}</span></div>`;
-//     // })
-//     // b.forEach(element =>{
-//     //     expenseData.innerHTML += `<div><span>${element}</span></div>`;
-//     // })
-
-
-    
-// }
-
-
-
-// function pushToObject() {
-    
-//     let selectedValue = select.options[select.selectedIndex].value;
-//     // console.log(selectedValue)
-//     if (selectedValue === 'income') {
-//         accountBalance.income.description.push(itemDescription.value);
-//         accountBalance.income.amount.push(parseInt(itemAmount.value));
-
-//     } else if (selectedValue === 'expense') {
-//         accountBalance.expense.description.push(itemDescription.value);
-//         accountBalance.expense.amount.push(parseInt(itemAmount.value));
-//     } else {
-//         console.log('Please select an option');
-//     }
-    
-// }
-
-// function checkBalance() {
-
-//     let sumIncome = 0;
-//     let sumExpense = 0;
-
-//     accountBalance.income.amount.forEach(element => {
-//         sumIncome += element;
-//     });
-
-//     accountBalance.expense.amount.forEach(element => {
-//         sumExpense += element;
-//     });
-
-//     let balance = sumIncome - sumExpense
-//     console.log(balance);
-//     sectionBalance.innerHTML = `<span>Your current balance is: ${balance}€</span>`;
-//     return balance;
-// }
-
-// addButton.addEventListener('click', function () {
-//     pushToObject();
-//     checkBalance();       
-// })
-
-// getFromLocal();
 
 function displayDateTime(){
     var myDate = new Date();
@@ -261,6 +159,15 @@ function displayDateTime(){
     var setDate = `The date is ${dd}/${mm}/${yyyy} ${hrs}:${min} `;
     
     return setDate;
+}
+
+if(localStorage.length === 0 || localStorage.length == null ) {
+    localStorage.setItem('incomes', JSON.stringify(accountBalance.incomes, undefined, 2));
+    localStorage.setItem('expenses', JSON.stringify(accountBalance.expenses, undefined, 2));
+    
+} else {
+    accountBalance.getIncomeData()
+    accountBalance.getExpenseData()
 }
 
 
